d2caf8e34cc4d3100bbe5674418caacc5e8432b5
Shivam Shah <shivik2541@gmail.com>
2021-07-08 04:22:41 +0530

    Day 1 complete

--- app.js
+++ app.js
@@ -4,9 +4,9 @@
   const https = require("https");
 
   const app = express();
-
+  var items =["Buy Food", "Cook Food", "Eat Food"];
   app.set("view engine", "ejs");
-
+  app.use(bodyParser.urlencoded({extended:true}));
   app.get("/", function(req, res) {
     var today = new Date();
 
@@ -19,9 +19,16 @@
     var day =today.toLocaleDateString("en-US",options);
 
     res.render("list",{
-      kindOfDay:day
+      kindOfDay:day,
+      newListItems:items
     });
 
+  });
+
+  app.post("/",function(req,res){
+   var item= req.body.newItem;
+   items.push(item);
+   res.redirect("/");
   });
 
   app.listen(3000, function() {
